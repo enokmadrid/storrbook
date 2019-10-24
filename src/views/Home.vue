1d52253a50ffd7b4fbb56f0314a74dbb31b24744
Enok Madrid <enokslaboratory@gmail.com>
2019-10-23 22:08:46 -0700

    setup scss loader for global sass variables

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="admin">
+    <header class="admin__header">
+      <a href="#" class="logo">
+        <img alt="Vue logo" src="../assets/logo_storrbook.svg" />
+      </a>
+      <div class="toolbar">
+        <a href="#"><i class="fa fa-bell"></i></a>
+        <a href="#"><i class="fa fa-user"></i></a>
+      </div>
+    </header>
+    <navigation class="admin__nav"></navigation>
+    <main class="admin__main">
+      <h2>Dashboard</h2>
+    </main>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  name: "home"
 };
 </script>
